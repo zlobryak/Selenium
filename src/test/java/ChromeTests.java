f0b211b917e215cc6813131f095fb249acae42cc
Zlobryak <clychnik@gmail.com>
2023-05-01 18:35:25 +0300

    Tests added. Headless mode on. .appveyor.yml adedd

---
+++ src/test/java/ChromeTests.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChromeTests {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Your test logic here
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input"))
+                .sendKeys("Василий Уткин");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input"))
+                .sendKeys("+79062421277");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']"))
+                .click();
+        driver.findElement(By.tagName("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(actual, expected);
+    }
+
+}
